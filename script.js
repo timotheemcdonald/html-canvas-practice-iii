71d0c93389f481c2eda6c8b3236eef4c51289d49
timotheemcdonald <timotheemcdonald@gmail.com>
2021-02-15 12:52:13 -0500

    added game functions

--- script.js
+++ script.js
@@ -34,3 +34,58 @@ addEventListener("keydown", function(e){
 addEventListener("keyup", function(e) {
     delete keysDown[e.key];
 }, false);
+
+//New Game Function
+let reset = function(){
+    hero.x = canvas.width /2;
+    hero.y = canvas.height /2;
+//randomly spawn monster position
+    monster.x = 32 + (Math.random() * (canvas.width - 64));
+    monster.y = 32 + (Math.random() * (canvas.height - 64));
+}
+
+//update game objects
+let update = function(modifier){
+    if(ArrowUp in keysDown){
+        hero.y -= hero.speed * modifier;
+    };
+    if(ArrowDown in keysDown){
+        hero.y += hero.speed * modifier;
+    };
+    if(ArrowLeft in keysDown){
+        hero.x -= hero.speed * modifier;
+    };
+    if(ArrowRight in keysDown){
+        hero.x += hero.speed * modifier;
+    };
+
+    if (
+        hero.x <= (monster.x +32)
+        && monster.x <= (hero.x + 32)
+        && hero.y <= (monster.y + 32)
+        && monster.y <= (hero.y + 32)
+    ){
+        ++monstersCaught;
+        reset();
+    }
+};
+
+//render to screen
+let render = function(){
+    if(bgReady){
+        ctx.drawImage(bgImage, 0, 0);
+    }
+    if(heroReady){
+        ctx.drawImage(heroImage, hero.x, hero.y);
+    }
+    if(monsterReady){
+        ctx.drawImage(monsterImage, monster.x, monster.y);
+    }
+
+    //Score
+    ctx.fillStyle = "rgb(250, 250, 250)";
+    ctx.font = "24px Helvetica";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("Monsters caught: " + monstersCaught, 32, 32);
+};
