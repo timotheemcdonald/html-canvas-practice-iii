8bd18831b0ab1186c8dba8ce5271d712735fd8c0
timotheemcdonald <timotheemcdonald@gmail.com>
2021-02-15 12:44:10 -0500

    added graphics and e.key event

--- script.js
+++ script.js
@@ -2,3 +2,35 @@ let canvas = document.getElementById('canvas1');
 let ctx = canvas.getContext('2d');
 canvas.width = 512;
 canvas.height = 480;
+
+let bgReady = false;
+let bgImage = new Image();
+bgImage.onload = function (){
+    bgReady = true;
+};
+bgImage.src = "./background.png"
+
+let hero = {
+    speed: 256,
+    x: 0,
+    y: 0,
+};
+
+let monster = {
+    x: 0,
+    y: 0
+};
+
+let monstersCaught = 0;
+
+let keysDown = {};
+
+//e.keycode is deprecated use e.key instead
+addEventListener("keydown", function(e){
+    keysDown[e.key] = true;
+    console.log(e.key)
+}, false);
+
+addEventListener("keyup", function(e) {
+    delete keysDown[e.key];
+}, false);
